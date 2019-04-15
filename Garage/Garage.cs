7c162db85f616d32a7de1cbaa22c3b7ba3336f1a
Ettrig <rolf.engstrand@gmail.com>
2019-04-15 10:46:47 +0200

    Buildable

--- Garage/Garage.cs
+++ Garage/Garage.cs
@@ -9,36 +9,90 @@ namespace GarageProject
     class Garage<T> : IEnumerable<T> where T : Vehicle
     {
         private int capacity;
-        public int Capacity {get => capacity; set => capacity=value;}
+        public int Capacity { get => capacity; set => capacity = value; }
         private int count;
-        public int Count {get => count; set => count=value;}
-        private Vehicle[] vehicles;
+        public int Count { get => count; set => count = value; }
+        protected Vehicle[] vehicles;
 
         public Garage(int cap)
         {
-            capacity = cap; 
+            capacity = cap;
             vehicles = new Vehicle[capacity];
         }
 
-        private class EnumeratorGarage
-
-        public void AddVehicle(Vehicle theVehicle)
+        class GarageEnumerator<Vehicle> : IEnumerator<Vehicle>
         {
-            int i=0; 
-            while (vehicles[i] != null) i++;
-            vehicles[i] = theVehicle; 
+            Vehicle[] vehicls;
+            int currentIndex = -1; 
+
+            public GarageEnumerator(Vehicle[] vehicles)
+            {
+                vehicls = vehicles; 
+            }
+
+            public Vehicle Current
+            {
+                get
+                {
+                    if (currentIndex == -1) throw new InvalidOperationException();
+                    else return vehicls[currentIndex];
+                }
+            }
+
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool MoveNext()
+            {
+                if (currentIndex == vehicls.Length) return false;
+                currentIndex = currentIndex+1;
+                while (currentIndex < vehicls.Length && vehicls[currentIndex] == null)
+                {
+                    currentIndex = currentIndex + 1;
+                }
+                if (currentIndex == vehicls.Length) return false;
+                else return true; 
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
         }
 
         // It is stated that T is Vehicle in this class
-        public IEnumerator<Vehicle> GetEnumerator()
+        public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return (IEnumerator<T>) new GarageEnumerator<Vehicle>(vehicles);
         }
 
- //       IEnumerator IEnumerable.GetEnumerator() 
- //       {
- //           return (IEnumerator) GetEnumerator();
- //       }
+        private IEnumerator GetEnumerator1()
+        {
+            return this.GetEnumerator();
+        }
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator1();
+        }
+
+        public bool AddVehicle(Vehicle theVehicle)
+        {
+            int i=0; 
+            // GarageHandler 
+            while (i<vehicles.Length && vehicles[i] != null) i++;
+            if (i >= vehicles.Length) return false;
+            else
+            {
+                vehicles[i] = theVehicle;
+                count = count + 1;
+                return true;
+            } 
+        }
     }
 }
